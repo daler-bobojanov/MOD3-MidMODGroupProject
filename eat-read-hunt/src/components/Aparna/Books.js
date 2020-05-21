fdcce520344be3655ada9d01b6e2c13bdaf65456
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-05-20 23:38:52 -0400

    comments added and code clean-up

--- eat-read-hunt/src/components/Aparna/Books.js
+++ eat-read-hunt/src/components/Aparna/Books.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { CircleArrow as ScrollUpButton } from 'react-scroll-up-button';
 
 const API_URL = 'https://bookauthority.org/api/site/get-topic-data?topicId=rest_api&viewId=best&formatId=print&featureId=null&skip=5&limit=10&getAdditionalBooks=false&getInfo=false&pageviewId=56023790858211ea9e710d1d42024a5c';
 
 class Books extends Component {
-
     state = {
         books: [],
         favBooks: []
@@ -14,7 +13,6 @@ class Books extends Component {
     constructor(props) {
         super(props);
         this.getData = this.getData.bind(this);
-
     }
 
     componentDidMount() {
@@ -33,7 +31,7 @@ class Books extends Component {
         return;
     }
     getData() {
-        const url = `${API_URL}/users/`;
+        const url = `${'https://cors-anywhere.herokuapp.com/'}${API_URL}/users/`;
         axios.get(url).then(response => response.data)
             .then((data) => {
                 this.setState({ books: data.books })
@@ -44,15 +42,15 @@ class Books extends Component {
         return (
             <div className="div_class">
                 {this.state.books.map((book) => (
-                    <div className="results-container">
-                        <table class="table table-dark">
+                    <div className="results-container" key={book.id}>
+                        <table className="table table-dark">
                             <thead>
                                 <th scope="col">Book</th>
                                 <th scope="col">Title</th>
                             </thead>
                             <tr scope="row">
                                 <td>{<img id={book._id} src={book.images.front.l.url} alt="No value" />}</td>
-                                <td class="text-white-50 align-text-bottom" >{book.title}<br>
+                                <td className="text-white-50 align-text-bottom" >{book.title}<br>
                                 </br>${book.price}
                                     <button onClick={this.saveBook.bind(this, book._id, book.images.front.l.url, book.title)}>Save</button>
                                 </td>
