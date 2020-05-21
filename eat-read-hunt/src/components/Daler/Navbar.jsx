6f4420d436c4938536a2e185f46d75ce9b761ba4
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-05-20 23:38:02 -0400

    comments added and code clean-up

--- eat-read-hunt/src/components/Daler/Navbar.jsx
+++ eat-read-hunt/src/components/Daler/Navbar.jsx
@@ -1,16 +1,15 @@
-// Daler's App imports
 import React from 'react';
-// import '../../styles/Daler/Navbar.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import homeBtn from '../../assets/Daler/homeBtn.png';
 
+// Daler's App imports
+// import '../../styles/Daler/Navbar.css';
+// import homeBtn from '../../assets/Daler/homeBtn.png';
+// import UserName from './UserName';
+// import SearchForm from './SearchForm';
 import AllJobs from './AllJobs';
 import MySavedJobs from './MySavedJobs';
 import Logo from './Logo';
 import TechNews from './TechNews';
-
-// import UserName from './UserName';
-// import SearchForm from './SearchForm';
 import HomePage from '../Homepage/HomePage';
 // ================================================================
 // Aleh's App imports
@@ -23,6 +22,7 @@ import Books from '../Aparna/Books';
 import Savebooks from '../Aparna/Savebooks';
 import Search from '../Aparna/Search';
 
+
 class Navbar extends React.Component {
     // function to Refresh the page upon Home button click
     refreshPage = () => {
@@ -32,39 +32,18 @@ class Navbar extends React.Component {
     render() {
         return (
             <Router>
-
-                {/* <nav>
-                    <ul className="nav-links">
-                        <li onClick={this.refreshPage}>
-                            <Link className="link" to="/"><img src={homeBtn} id='nav-img' alt="home-button" /></Link>
-                        </li>
-                        <li>
-                            <Link className="link" to="/alljobs/#alljobs" target="_top">All jobs</Link>
-                        </li>
-                        <li>
-                            <Link className="link" to="/mysavedjobs/#mysavedjobs" target="_top">My Saved jobs</Link>
-                        </li>
-                        <li>
-                            <Link className="link" to="/technews/#technews" target="_top">Tech News</Link>
-                        </li>
-                        <li>
-                            User Name
-                        </li>
-                    </ul>
-                </nav>  */}
-
                 <Switch>
+                    {/* Job Search section */}
                     <Route exact path="/" component={HomePage} />
                     <Route exact path="/searchjobs" component={Logo} />
                     <Route exact path='/alljobs' component={AllJobs} />
                     <Route exact path='/mysavedjobs' component={MySavedJobs} />
                     <Route exact path="/technews" component={TechNews} />
-
-
+                    {/* Recipes section */}
                     <Route exact path="/searchrecipe" component={App} />
                     <Route exact path="/top" component={Top} />
                     <Route exact path="/motivation" component={Motivation} />
-
+                    {/* Book Store section */}
                     <Route exact path="/books" component={Books} />
                     <Route exact path="/savebooks" component={Savebooks} />
                     <Route exact path="/search" component={Search} />
