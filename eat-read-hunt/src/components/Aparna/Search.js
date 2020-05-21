6ac0e40199c3193e14c9df85162e57c1536695c8
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-05-20 23:38:28 -0400

    comments added and code clean-up

--- eat-read-hunt/src/components/Aparna/Search.js
+++ eat-read-hunt/src/components/Aparna/Search.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import ReactDom from 'react-dom'
+// import ReactDom from 'react-dom'
 import axios from 'axios';
 import { CircleArrow as ScrollUpButton } from 'react-scroll-up-button';
+import '../../styles/Aparna/App.css';
 
 const API_URL = 'https://bookauthority.org/api/site/get-topic-data?topicId=rest_api&viewId=best&formatId=print&featureId=null&skip=5&limit=10&getAdditionalBooks=false&getInfo=false&pageviewId=56023790858211ea9e710d1d42024a5c';
 
@@ -14,12 +15,9 @@ class Search extends Component {
         //alert('a')
         super(props);
 
-
         this.getData = this.getData.bind(this);
         this.setSearchVal = this.setSearchVal.bind(this);
     }
-
-
 
     setSearchVal(searchbox) {
         console.log(searchbox.target.value);
@@ -28,16 +26,14 @@ class Search extends Component {
 
     getData() {
         alert(this.state.searchString);
-        const url = `${API_URL}/users/`;
+        const url = `${'https://cors-anywhere.herokuapp.com/'}${API_URL}/users/`;
         axios.get(url).then(response => response.data)
             .then((data) => {
                 this.setState({ books: data.books })
             })
     }
 
-
     render() {
-
         return (
             <div className="div_first">
                 <h1><label className="label"> Search</label></h1>
@@ -46,7 +42,7 @@ class Search extends Component {
                     <button onClick={this.getData} className="btn btn-primary">submit</button>
                 </div>
                 {this.state.books.map((book) => (
-                    <div className="results-container">
+                    <div className="results-container" key={book.id}>
                         {book._id}
                         <br></br>
                         <img id={book._id} src={book.images.front.l.url} alt="No value" />
@@ -54,10 +50,8 @@ class Search extends Component {
                 ))}
                 <ScrollUpButton />
             </div>
-
         )
     }
-
 }
 export default Search;
 
