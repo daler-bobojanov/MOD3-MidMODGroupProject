24fc11e674487c6c3ad7c63832b9ab0d61a6ff88
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-05-20 23:38:40 -0400

    comments added and code clean-up

--- eat-read-hunt/src/components/Aparna/Savebooks.js
+++ eat-read-hunt/src/components/Aparna/Savebooks.js
@@ -1,44 +1,44 @@
-import Books from './Books.js'
+// import Books from './Books.js'
 import React, { Component } from 'react';
 
 class Savebooks extends Component {
-    state = { 
-        favBooks : []
+    state = {
+        favBooks: []
     }
 
-    componentWillMount(){
+    componentWillMount() {
         //alert(localStorage.getItem('data'));
-       // this.state.favBooks = JSON.parse(localStorage.getItem('data'));
+        // this.state.favBooks = JSON.parse(localStorage.getItem('data'));
         localStorage.getItem('data') && this.setState({
             favBooks: JSON.parse(localStorage.getItem('data')),
-            isLoading : false
+            isLoading: false
         })
         alert(this.state.favBooks.length);
         //this.getData();
     }
-render() {
-    return (
-        <div className="div_class">
-           <h2> My Favourite Books </h2>  
-            {this.state.favBooks.map((book) => (
-                <div className="results-container">
-                    <table class="table table-dark">
-                        <thead>
-                            <th scope="col">Book</th>
-                            <th scope="col">Title</th>
-                        </thead>
-                        <tr scope="row">
-                        <td>{<img id={book._id} src={book.url} alt="No value" />}</td>
-                        <td class="text-white-50 align-text-bottom" >{book.title}
-                        </td>
-                        </tr>
-                    </table>
-                </div>
-            ))}
-        </div>
+    render() {
+        return (
+            <div className="div_class">
+                <h2> My Favourite Books </h2>
+                {this.state.favBooks.map((book) => (
+                    <div className="results-container">
+                        <table class="table table-dark">
+                            <thead>
+                                <th scope="col">Book</th>
+                                <th scope="col">Title</th>
+                            </thead>
+                            <tr scope="row">
+                                <td>{<img id={book._id} src={book.url} alt="No value" />}</td>
+                                <td class="text-white-50 align-text-bottom" >{book.title}
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                ))}
+            </div>
 
-)
-}
+        )
+    }
 }
 
 export default Savebooks;
